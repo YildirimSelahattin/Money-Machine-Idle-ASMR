190f46d6fac68989d4d30ed8ca669a4b112971ac
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2022-12-22 09:32:18 +0300

    Added UI Pack

---
+++ Assets/Script/ConveyorTexMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorTexMove : MonoBehaviour
+{
+    public float bgSpeed = 0.1f;
+    public Renderer bgRend;
+    public static int breake = 1;
+
+    void Start()
+    {
+        bgSpeed = 0.002f;
+    }
+
+    void Update()
+    {
+        bgRend.materials[0].mainTextureOffset += new Vector2(0,-breake * bgSpeed * Time.deltaTime);
+        if (bgRend.materials.Length > 1)
+        {
+            bgRend.materials[1].mainTextureOffset += new Vector2(0,-breake * bgSpeed * Time.deltaTime);
+        }
+    }
+}
+
+
+
