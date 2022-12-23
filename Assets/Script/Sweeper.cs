5b5f71a6582cdcdbf0760a8d8eb3e29ccf582244
Emirhan Kaya <aemirhan@sabanciuniv.edu>
2022-12-23 17:20:56 +0300

    Sweeper route changed

--- Assets/Script/Sweeper.cs
+++ Assets/Script/Sweeper.cs
@@ -25,11 +25,13 @@ public class Sweeper : MonoBehaviour
             if (GameDataManager.Instance.gridArray[i] > 0)
             {
                 myPos = GameManager.Instance.gridParent.transform.GetChild(i).transform.GetChild(2).transform.position;
+                myPos.y +=1f;
+                myPos.z +=0.4f;
                 myCube.transform.DOMove(myPos,1f).OnComplete(()=>{
                 i++;
                 if (i == GameDataManager.Instance.gridArray.Length||GameDataManager.Instance.gridArray[i] < 0)
                 {
-                    myCube.transform.DOMove(new Vector3(0,0,0),2f);
+                    myCube.transform.DOMove(new Vector3(0,2,-20),2f);
                 }
                 
                 else{CheckGrid(i);}
