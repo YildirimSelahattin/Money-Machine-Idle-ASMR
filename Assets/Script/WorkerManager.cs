b5e7d5f1f93f7cdfbac35679b0f18f53cecccd58
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2022-12-28 11:40:42 +0300

    worker move inProgress

--- Assets/Script/WorkerManager.cs
+++ Assets/Script/WorkerManager.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.EventSystems;
 
 public class WorkerManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public  int maxComeAndGoCounter = 8;
+    private float _baseSpeed;
+    public  float addedTimeWhileGoing;
+    public float wheelBorrowCapacity = 10;
+    public int countedUntilSleep=0;
+    
     void Start()
     {
         
@@ -16,4 +23,28 @@ public class WorkerManager : MonoBehaviour
     {
         
     }
+    
+    public void MoveMachineAndComeBackByIndex(int index)
+    {
+        
+        transform.DOLocalMove(GameManager.Instance.gridParent.transform.GetChild(index).position, _baseSpeed+addedTimeWhileGoing).SetEase(Ease.InOutBack).OnComplete(() =>
+        {
+            StartCoroutine(GameManager.Instance.gridParent.transform.GetChild(index).transform.GetChild(3).gameObject.GetComponent<MachineManager>().WaitAndPrint(MachineManager.Instance.countWaitTime));
+            transform.DOLocalMove(Spawner.Instance._spwanPos, _baseSpeed).OnComplete(
+                () =>
+                {
+                    countedUntilSleep++;
+                    if (countedUntilSleep > maxComeAndGoCounter)
+                    {
+                        //GoSleep();
+                    }
+                    else
+                    {
+                        Spawner.Instance.workerList.Push(gameObject);
+                        Spawner.Instance.LookForEmptyMachine();
+                    }
+                });
+        }) ;
+    }
+    
 }
